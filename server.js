b7e78180eb8b69096eb427467cb8f846c4e067eb
Anu-Asok <anu.asok701@gmail.com>
2016-09-25 10:22:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,8 +66,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName',function(req,res){
-	res.send(create_template(articles[articleName]));
+app.get('/article-one',function(req,res){
+	res.send(create_template(articles[article-one]));
 });
 
 app.get('/ui/style.css', function (req, res) {

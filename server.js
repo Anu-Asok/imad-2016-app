edf44aca07710702b99cc442c76cb5b6070b55c8
Anu-Asok <anu.asok701@gmail.com>
2016-09-25 09:24:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,84 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+    'article-one':{
+        'heading':'Article One | ASP',
+        'title':'Article One',
+        'date':'Sep 23 2016',
+        'content':`<p>
+                    This is the first article.This is the first article.This is the first article.
+                    This is the first article.This is the first article.This is the first article.
+                </p>
+                <p>
+                    This is the first article.This is the first article.This is the first article.
+                    This is the first article.This is the first article.This is the first article.
+                </p>
+                <p>
+                    This is the first article.This is the first article.This is the first article.
+                    This is the first article.This is the first article.This is the first article.
+                </p>`
+    },
+    'article-two':{
+		'heading':'Article Two | ASP',
+        'title':'Article Two',
+        'date':'Sep 24 2016',
+        'content':`<p>
+                This is the second article.
+                </p>`
+	},
+    'article-three':{
+		'heading':'Article three | ASP',
+        'title':'Article Three',
+        'date':'Sep 24 2016',
+        'content':`<p>
+                This is the third article.
+                </p>`
+	}
+};
+
+function create_template(data){
+    
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+    var htmltemplate=
+    `<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name='viewport' content='width=device-width,initial-scale=1'/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+    </html>`;
+    return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
+/*app.get('/article-one',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
@@ -19,6 +92,9 @@ app.get('/article-two',function(req,res){
 
 app.get('/article-three',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});*/
+app.get('/:articlename',function(req,res){
+	res.send(create_template(articles[articlename]));
 });
 
 app.get('/ui/style.css', function (req, res) {

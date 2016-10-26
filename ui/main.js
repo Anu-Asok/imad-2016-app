c32cde0a6f9af73d355d3a6a1ebfcc9c48ee7d8c
Anu-Asok <anu.asok701@gmail.com>
2016-10-26 20:42:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ submit.onclick=function(){
         for(var i=0;i<names.length;i++)
           list += '<li>'+names[i]+'</li>';
         var ui=document.getElementById('namelist');
-        ui.innerHTML=names;
+        ui.innerHTML=list;
       }
     }
   }

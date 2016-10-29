54fb9393db52f2541e249e12c8d59e135e2d6e6a
Anu-Asok <anu.asok701@gmail.com>
2016-10-29 21:36:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config={
+    user:"anu-asok",
+    database:"anu-asok",
+    host:"db.imad.hasura-app.io",
+    port:"5432",
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -102,6 +109,16 @@ app.get('/submit-name',function(req,res){
 	var name=req.query.name;
 	names.push(name);
 	res.send(JSON.stringify(names));
+});
+
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+      pool.query('SELECT * FROM test',function(err,result){
+         if(err)
+            res.status(500).send(err.toString());
+        else
+            res.send(JSON.stringify(result));
+      });
 });
 
 app.get('/:user-name',function(req,res){

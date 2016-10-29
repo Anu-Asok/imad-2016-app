20059e1d08d3ad6e4c1afadde39bdea6a2c97012
Anu-Asok <anu.asok701@gmail.com>
2016-10-29 22:00:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,9 +132,20 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
-app.get('/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
     var articleName=req.params.articleName;
-	res.send(create_template(articles[articleName]));
+    pool.query("SELECT * FROM articles WHERE title="+articleName,function(err,result){
+       if(err)
+        res.status(500).send(err.toString());
+       else{
+           if(result.rows.length === 0)
+            res.status(404).send("Article not found");
+           else{
+               var articleData=result.rows[0];
+               res.send(create_template(articleData));
+           }
+       }    
+    });
 });
 
 

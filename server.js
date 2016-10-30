47e617471830e835d82c8b313b4ad8159c720ac8
Anu-Asok <anu.asok701@gmail.com>
2016-10-30 21:40:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,8 +56,7 @@ function create_template(data){
         <script type="text/javascript">
         var button = document.getElementById('likes');
         button.onclick=function(){
-          pool.query("UPDATE articles SET likes = likes + 1 WHERE title=$1",[title],function(err,result){
-                res.status(500).send(err.toString());});
+          pool.query("UPDATE articles SET likes = likes + 1 WHERE title=$1",[title],function(err,result){});
           pool.query("SELECT likes FROM articles WHERE title=$1",[title],function(err,result){
                if(err)
                 res.status(500).send(err.toString());
@@ -105,7 +104,8 @@ app.get('/submit-name',function(req,res){
 
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
-      pool.query('SELECT * FROM test',function(err,result){
+        var title="article-one";
+      pool.query('UPDATE articles SET likes=likes+1 WHERE title=$',[title],function(err,result){
          if(err)
             res.status(500).send(err.toString());
         else

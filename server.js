bba77611d703c4c15ab82875ea8e3c2233737f5e
Anu-Asok <anu.asok701@gmail.com>
2016-10-30 21:43:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ var pool=new Pool(config);
 app.get('/test-db',function(req,res){
         var title="article-one";
       pool.query('UPDATE articles SET likes=likes+1 WHERE title=$1',[title],function(err,result){
-  
+        //Good job
       });
 });
 

1b725729134a8eb9361dfb77fb1654e9fac35b86
Anu-Asok <anu.asok701@gmail.com>
2016-11-07 18:14:41 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,3 @@
-var url="anu-asok.imad.hasura-app.io";
-
 var button = document.getElementById('click');
 button.onclick=function(){
   var request=new XMLHttpRequest();
@@ -12,7 +10,7 @@ button.onclick=function(){
             }
           }
         }
-        request.open('GET',`http://${url}/counter`,true);
+        request.open('GET',"/counter",true);
         request.send(null);
 };
 
@@ -35,6 +33,36 @@ submit.onclick=function(){
   }
   var nameInput=document.getElementById('name');
   var name=nameInput.value;
-  request.open('GET',`http://${url}/submit-name?name=`+name,true);
+  request.open('GET',"/submit-name?name="+name,true);
   request.send(null);
+};
+
+
+var submit_btn=document.getElementById('submit-btn');
+submit_btn.onclick=function(){
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+      if(request.status === 200){
+		console.log('User logged in');
+		alert('Logged in successfully!');
+	  }
+	  else if(request.status===403){
+		alert('Username/password is incorrect');	
+		console.log("no entry");
+	  }
+	  
+	  else if(request.status===500){
+		alert('Something went wrong!');	
+		console.log("Wrong");
+	  }
+    }
+  }
+  var username=document.getElementById('username').value;
+  var password=document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST',"/login",true);
+  request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.stringify({username:username,password:password}));
 };
